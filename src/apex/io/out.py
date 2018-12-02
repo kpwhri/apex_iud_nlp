1a8bf96769b66fd8d03943b34b9c8bcb1e8278e4
dcronkite <dcronkite@gmail.com>
2018-12-02 14:36:43 -0800

    Refactored FileWrapper classes to improve reusability

--- src/apex/io/out.py
+++ src/apex/io/out.py
@@ -23,55 +23,63 @@ class NullFileWrapper:
         pass
 
 
-class CsvFileWrapper:
+class FileWrapper:
 
-    def __init__(self, file, path=None, **kwargs):
+    def __init__(self, file, path=None, header=None, **kwargs):
         if path:
             self.fp = os.path.join(path, file)
             os.makedirs(path, exist_ok=True)
         else:
             self.fp = file
         self.fh = None
+        self.header = header or []
+
+    def __enter__(self):
+        if self.fp:
+            self.fh = open(self.fp, 'w')
+            self.writeline(self.header)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.fh:
+            self.fh.close()
+
+    def writeline(self, line, sep=None):
+        if sep:
+            self.fh.write(sep.join(str(s) for s in line) + '\n')
+        else:
+            self.fh.write(str(line) + '\n')
+
+
+class CsvFileWrapper(FileWrapper):
+
+    def __init__(self, file, path=None, header=None, **kwargs):
+        super().__init__(file, path=path, header=header, **kwargs)
         self.writer = None
 
     def __enter__(self):
         if self.fp:
             self.fh = open(self.fp, 'w', newline='')
             self.writer = csv.writer(self.fh)
-            self.writeline(['name', 'algorithm', 'value'])
+            self.writeline(self.header)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.fh:
             self.fh.close()
 
-    def writeline(self, line):
+    def writeline(self, line, **kwargs):
         if self.writer:
             self.writer.writerow(line)
 
 
-class TsvFileWrapper:
+class TsvFileWrapper(FileWrapper):
 
-    def __init__(self, file, path=None, **kwargs):
-        if path:
-            self.fp = os.path.join(path, file)
-            os.makedirs(path, exist_ok=True)
-        else:
-            self.fp = file
-        self.fh = None
+    def __init__(self, file, path=None, header=None, **kwargs):
+        super().__init__(file, path=path, header=header, **kwargs)
 
-    def __enter__(self):
-        if self.fp:
-            self.fh = open(self.fp, 'w', newline='')
-            self.writeline(['name', 'algorithm', 'status', 'result', 'matches', 'text'])
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        if self.fh:
-            self.fh.close()
-
-    def writeline(self, line):
-        self.fh.write('\t'.join(str(s) for s in line) + '\n')
+    def writeline(self, line, sep='\t'):
+        super().writeline(line, sep=sep)
 
 
 class TableWrapper:
@@ -98,7 +106,7 @@ def get_file_wrapper(name=None, kind=None, path=None,
         return NullFileWrapper()
     name = name.replace('{datetime}', DATETIME_STR)
     if kind == 'csv':
-        return CsvFileWrapper(name, path)
+        return CsvFileWrapper(name, path, header=['name', 'algorithm', 'value'])
     elif kind == 'sql':
         return TableWrapper(name, driver, server, database)
     else:
@@ -109,4 +117,6 @@ def get_logging(directory='.', ignore=False):
     if ignore:
         return NullFileWrapper()
     else:
-        return TsvFileWrapper(path=directory, file=f'text_{DATETIME_STR}.out')
+        return TsvFileWrapper(path=directory,
+                              file=f'text_{DATETIME_STR}.out',
+                              header=['name', 'algorithm', 'status', 'result', 'matches', 'text'])
