a8db337ed22b81a9ba7180c51885379af04fff2e
dcronkite <dcronkite@gmail.com>
2018-12-08 16:04:12 -0800

    Update to expulsion and perforation algorithms
    * displacement
    * possible perforation
    * complete perf
    * laparoscopic removal
    * visualized
    * possible expulsion

--- src/apex/algo/iud_expulsion.py
+++ src/apex/algo/iud_expulsion.py
@@ -2,28 +2,39 @@ from apex.algo.iud_perforation import IUD
 from apex.algo.pattern import Document, Pattern
 from apex.algo.result import Status, Result
 
-hypothetical = r'(\bif\b|\bnose\b|contact|doctor|should|can|possib|includ|failure|' \
+
+possible = r'\b(unlikely|possib(ly|le|ility)|improbable|potential|suspect|chance|may\b|afraid|concern|tentative)'
+hypothetical = r'(\bif\b|\bnose\b|contact|doctor|should|can|includ|failure|' \
                r'how|call|rare|risk|associated|avoid)'
 historical = r'(hx|history|previous)'
 negation = r'(\bnot\b|\bor\b|\bno\b)'
 
+POSSIBLE = Pattern(possible)
 INCORRECT = Pattern(r'(incorrect(ly)?|poor(ly)?|wrong(ly)?|bad|\bmal\b)',
                     negates=[hypothetical, negation])
 PLACEMENT = Pattern(r'(plac\w+|position\w*|location)',
                     negates=[hypothetical, negation])
-MALPOSITION = Pattern(f'(mal-(position|place))',
+MALPOSITION = Pattern(r'(mal (position|place)|trans located)',
                       negates=[hypothetical, negation])
-EXPULSION = Pattern(f'(expel|expuls)', negates=[hypothetical, historical, negation])
-DEF_EXPULSION = Pattern(r'spontan\w+ (expel|expul)')
-PARTIAL_EXP = Pattern(f'(partial\w* (expel|expul)|(expel|expul)\w* partial|'
-                      f'dislodg|({IUD})( was)? (seen|visualiz))',
+DISPLACEMENT = Pattern(r'(\brotat\w+|(lower|inferior) (uter(ine|us)|cervi(x|cal))|'
+                       r'displac\w+|dislodg\w+)',
+                       negates=[hypothetical, negation, historical])
+EXPULSION = Pattern(r'(expel|expuls)',
+                    negates=[hypothetical, historical, negation])
+DEF_EXPULSION = Pattern(r'spontan\w+ (expel|expul)',
+                        negates=[hypothetical, historical, negation])
+PARTIAL_EXP = Pattern(r'(partial\w* (expel|expul)|(expel|expul)\w* partial)',
                       negates=[negation, 'strings? of'])
-PROTRUDES = Pattern(r'protrud', negates=['strings?', hypothetical])
+VISUALIZED = Pattern(r'({IUD})( was)? (seen|visualiz)',
+                     negates=[negation, hypothetical])
+PROTRUDES = Pattern(r'protrud',
+                    negates=['strings?', hypothetical])
 LOST = Pattern(r'(toilet|fell)')
 MISSING = Pattern(r'(missing|lost|can(no|\W)?t feel)',
                   negates=[hypothetical])
 STRINGS = Pattern(r'strings?', negates=['bothersome'])
-ANY = (PLACEMENT, MALPOSITION, EXPULSION, LOST, MISSING)
+ANY = (PLACEMENT, MALPOSITION, EXPULSION, LOST, MISSING,
+       PROTRUDES, VISUALIZED, PARTIAL_EXP, DEF_EXPULSION)
 
 
 class ExpulsionStatus(Status):
@@ -34,6 +45,9 @@ class ExpulsionStatus(Status):
     MISSING_STRING = 4
     PARTIAL = 5
     LOST = 6
+    DISPLACEMENT = 7
+    POSSIBLE = 8
+    POSS_DISPLACEMENT = 9
     SKIP = 99
 
 
@@ -51,7 +65,9 @@ def determine_iud_expulsion(document: Document):
                 yield ExpulsionStatus.MALPOSITION, section.text
                 found = True
             if section.has_patterns(EXPULSION):
-                if section.has_patterns(PARTIAL_EXP):
+                if section.has_patterns(POSSIBLE):
+                    yield ExpulsionStatus.POSSIBLE, section.text
+                elif section.has_patterns(PARTIAL_EXP):
                     yield ExpulsionStatus.PARTIAL, section.text
                 else:
                     yield ExpulsionStatus.EXPULSION, section.text
@@ -61,6 +77,12 @@ def determine_iud_expulsion(document: Document):
                 found = True
             if section.has_patterns(LOST):
                 yield ExpulsionStatus.LOST, section.text
+                found = True
+            if section.has_patterns(DISPLACEMENT):
+                if section.has_patterns(POSSIBLE):
+                    yield ExpulsionStatus.POSS_DISPLACEMENT, section.text
+                else:
+                    yield ExpulsionStatus.DISPLACEMENT, section.text
                 found = True
         # if not found:
         #     yield ExpulsionStatus.NONE, document.text
--- src/apex/algo/iud_perforation.py
+++ src/apex/algo/iud_perforation.py
@@ -7,23 +7,35 @@ brand = r'(mirena|paragu?ard|skyla\b|lilett?a|kyleena|copper)'
 embedded = r'([ie]mbedded|impacted)'
 # removed: |foreign body
 migrated = r'\b(stuck|migrat\w+|extrauterine|omentum|displac\w+|(intra)?peritoneal)'
-boilerplate = r'\b(complication|pamphlet|warning|information|review|side effect|counsel|sign|infection|ensure|cramps)'
-hypothetical = r'\b(unlikely|possib(ly|le|ility)|improbable|potential|if\b|suspect|chance|may\b|risk|afraid|concern)'
+boilerplate = r'\b(complication|pamphlet|warning|information|review|side effect|counsel|sign|infection|ensure|' \
+              r'cramps|risk|\bif\b)'
+possible = r'\b(unlikely|possib(ly|le|ility)|improbable|potential|suspect|chance|may\b|afraid|concern|tentative)'
 negation = r'(no evidence|without|r/o|rule out|normal|\bnot?\b|\bor\b)'
-history = r'(history of|previous)'
+history = r'(history of|previous|hx|past)'
 
 impact_neg = r'(cerumen|tympanic|ear|hormon\w+)'
 
-PERFORATION = Pattern(r'perforat(ion|ed|e)s?',
-                      negates=[boilerplate, history, hypothetical, negation, impact_neg])
-PARTIAL_PERFORATION = Pattern(r'partial(ly)? perforat(ion|ed|e)s?',
-                              negates=[boilerplate, history, hypothetical, negation, impact_neg])
+COMPLETE = Pattern('(intra (peritoneal|abdominal)|complete(ly)? perforat(ion|ed|e)s?)',
+                   negates=[boilerplate, history, negation, impact_neg])
+POSSIBLE = Pattern(possible)
+PERFORATION = Pattern(r'('
+                      r'perforat(ion|ed|e)s?|(pierc\w+|thr(ough|u))( the)?( uterine)? '
+                      r'(endometrium|wall|myometrium|serosa|perimetrium)'
+                      r')',
+                      negates=[boilerplate, history, negation, impact_neg])
+PARTIAL = Pattern(r'partial(ly)? perforat(ion|ed|e)s?',
+                  negates=[boilerplate, history, possible, negation, impact_neg])
 IUD = Pattern(f'({iuds}|{lng_iuds}|{brand})')
 EMBEDDED = Pattern(f'({embedded})',
-                   negates=[boilerplate, history, hypothetical, negation, impact_neg])
+                   negates=[boilerplate, history, possible, negation, impact_neg])
 MIGRATED = Pattern(f'{migrated}',
-                   negates=[boilerplate, history, hypothetical, negation])
-
+                   negates=[boilerplate, history, possible, negation])
+LAPAROSCOPIC_REMOVAL = Pattern(r'('
+                               r'(lap[ao]r[ao](scop|tom)|pelviscop)(\w+\W+){0,10} remov\w+|'
+                               r'remov\w+(\w+\W+){0,10}lap[ao]r[ao]scop\w+'
+                               r')',
+                               negates=[history, boilerplate])
+ALL = (COMPLETE, PERFORATION, EMBEDDED, MIGRATED, LAPAROSCOPIC_REMOVAL)
 years_ago = r'(?:\d+ (?:year|yr|week|wk|month|mon|day)s? (?:ago|before|previous))'
 date_pat = r'\d+[-/]\d+[-/]\d+'
 date2_pat = r'\d+[/]\d+'
@@ -38,45 +50,39 @@ class PerforationStatus(Status):
     EMBEDDED = 2
     UNKNOWN = 3
     MIGRATED = 4
-    PARTIAL_PERFORATION = 5
+    PARTIAL = 5
+    POSSIBLE = 6
+    LAPAROSCOPIC_REMOVAL = 7
+    COMPLETE = 8
     SKIP = 99
-
-
-def classify_result(res: PerforationStatus, date):
-    if res == PerforationStatus.PARTIAL_PERFORATION:
-        if date:
-            return 0
-        return 1
-    elif res == PerforationStatus.PERFORATION:
-        if date:
-            return 0
-        return 1
-    elif res == PerforationStatus.EMBEDDED:
-        if date:
-            return 0
-        return 1
-    return -1
 
 
 def confirm_iud_perforation(document: Document, expected=None):
     for value, text, date in determine_iud_perforation(document):
-        res = classify_result(value, date)
-        yield Result(value, res, expected, text, extras=date)
+        yield Result(value, value.value, expected, text, extras=date)
 
 
 def determine_iud_perforation(document: Document):
-    if document.has_patterns(PERFORATION, EMBEDDED, MIGRATED, ignore_negation=True):
+    if document.has_patterns(*ALL, ignore_negation=True):
         # see if any sentences that contain "IUD" also contain perf/embedded
         for section in document.select_sentences_with_patterns(IUD):
             date = section.get_pattern(DATE_PAT)
-            if section.has_patterns(PARTIAL_PERFORATION):
-                yield PerforationStatus.PARTIAL_PERFORATION, section.text, date
+            if section.has_patterns(COMPLETE):
+                yield PerforationStatus.COMPLETE, section.text, date
+            elif section.has_patterns(PARTIAL):
+                yield PerforationStatus.PARTIAL, section.text, date
             elif section.has_patterns(PERFORATION):
-                yield PerforationStatus.PERFORATION, section.text, date
+                if section.has_pattern(POSSIBLE):
+                    yield PerforationStatus.POSSIBLE, section.text, date
+                else:
+                    yield PerforationStatus.PERFORATION, section.text, date
             elif section.has_patterns(EMBEDDED):
                 yield PerforationStatus.EMBEDDED, section.text, date
             elif section.has_patterns(MIGRATED):
                 yield PerforationStatus.MIGRATED, section.text, date
+            # check for laparoscopic removal -> suggests complete perf
+            if section.has_patterns(LAPAROSCOPIC_REMOVAL):
+                yield PerforationStatus.LAPAROSCOPIC_REMOVAL, section.text, date
         yield PerforationStatus.NONE, document.text, None
     else:
         yield PerforationStatus.SKIP, None, None
